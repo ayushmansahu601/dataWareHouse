7e80181f2d6c4f20d939c3621b82de15e4add67f
ayushmansahu601 <ayushmansahu0601@gmail.com>
2025-08-13 05:50:26 +0530

    initial commit

---
+++ tests/test_third.sql
@@ -0,0 +1,51 @@
+/*
+===============================================================================
+Quality Checks
+===============================================================================
+Script Purpose:
+    This script performs quality checks to validate the integrity, consistency, 
+    and accuracy of the third Layer. These checks ensure:
+    - Uniqueness of surrogate keys in dimension tables.
+    - Referential integrity between fact and dimension tables.
+    - Validation of relationships in the data model for analytical purposes.
+
+Usage Notes:
+    - Investigate and resolve any discrepancies found during the checks.
+===============================================================================
+*/
+
+-- ====================================================================
+-- Checking 'third.dim_customers'
+-- ====================================================================
+-- Check for Uniqueness of Customer Key in third.dim_customers
+-- Expectation: No results 
+SELECT 
+    customer_key,
+    COUNT(*) AS duplicate_count
+FROM third.dim_customers
+GROUP BY customer_key
+HAVING COUNT(*) > 1;
+
+-- ====================================================================
+-- Checking 'third.product_key'
+-- ====================================================================
+-- Check for Uniqueness of Product Key in third.dim_products
+-- Expectation: No results 
+SELECT 
+    product_key,
+    COUNT(*) AS duplicate_count
+FROM third.dim_products
+GROUP BY product_key
+HAVING COUNT(*) > 1;
+
+-- ====================================================================
+-- Checking 'third.fact_sales'
+-- ====================================================================
+-- Check the data model connectivity between fact and dimensions
+SELECT * 
+FROM third.fact_sales f
+LEFT JOIN third.dim_customers c
+ON c.customer_key = f.customer_key
+LEFT JOIN third.dim_products p
+ON p.product_key = f.product_key
+WHERE p.product_key IS NULL OR c.customer_key IS NULL  
